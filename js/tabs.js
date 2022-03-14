6fd528dcbdb3f462c375fe15cef3a929ff6c67c4
Роман Киленников <RomanKS@dengimigom.local>
2022-03-14 17:18:24 +0300

    fix tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.btn--person').forEach(function (stepBtn) {
+    document.querySelectorAll('.btn-person').forEach(function (stepBtn) {
         stepBtn.addEventListener('click', function (eventStep) {
             const path = eventStep.currentTarget.dataset.path;
-            document.querySelectorAll('.btn--person').forEach(function (btn) {
-                btn.classList.remove('btn--person-active')
+            console.log(path)
+
+            document.querySelectorAll('.btn-person').forEach(function (btn) {
+                btn.classList.remove('btn-person-active')
             });
-            eventStep.currentTarget.classList.add('person--active');
+            eventStep.currentTarget.classList.add('btn-person-active');
+
             document.querySelectorAll('.person').forEach(function (tabContent) {
-                tabContent.classList.remove('person--active')
+                tabContent.classList.remove('person-ac')
             });
-            document.querySelector(`[data-target="${path}"]`).classList.add('person--active')
+
+            document.querySelector(`[data-target="${path}"]`).classList.add('person-ac')
         });
     })
 })
