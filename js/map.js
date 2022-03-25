38a72c201987582fb756033c3904a945c53a9061
Роман Киленников <RomanKS@dengimigom.local>
2022-03-25 17:07:10 +0300

    fix

--- js/map.js
+++ js/map.js
@@ -3,20 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function init() {
         var myMap = new ymaps.Map("map", {
-            center: [48.872185073737896, 2.354223999999991],
-            zoom: 14,
-            controls: []
+            center: [55.75846806898367, 37.60108849999989],
+            zoom: 15,
+            controls: ['geolocationControl', 'zoomControl']
         }, {
             suppressMapOpenBlock: true
         });
 
-        var myPlacemark = new ymaps.Placemark([48.872185073737896, 2.354223999999991], {}, {
+        var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
             iconLayout: 'default#image',
-            iconImageHref: './img/subtract.svg',
-            iconImageSize: [30, 42],
-            iconImageOffset: [-15, -42]
+            iconImageHref: '../img/icon/subtract.svg',
+            iconImageSize: [20, 20],
+            iconImageOffset: [-5, -10]
         });
-
+        myMap.behaviors.disable('scrollZoom');
         myMap.geoObjects.add(myPlacemark);
     }
 });
